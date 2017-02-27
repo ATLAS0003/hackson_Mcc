de0595379561b7a73b9de574798b43ae4dad6891
ATLAS0003 <norihiromatsuzaka@yahoo.co.jp>
2017-02-27 23:09:01 +0900

    サンプルより[shutdown]以外を削除 おはよう->おはようございます　と返すだけです

--- Main.js
+++ Main.js
@@ -15,14 +15,6 @@ var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
 //文字コード変換
-/*include('./encording/encording.js');
-
-function include(fileName) {
-    var scriptE = document.createElement('script');
-    scriptE.type = 'text/javascript';
-    scriptE.src = fileName;
-    document.body.appendChild(scriptE);
-}*/
 var encoding = require('./encoding.js/encoding.js');
 //文字コード変換は使わないかもです
 var controller = Botkit.slackbot({
@@ -33,119 +25,10 @@ controller.spawn({
     token: process.env.token
 }).startRTM();
 
-controller.hears('weather', 'direct_mention', function (bot, message) {
-    console.log(message);
-    askWeather = function (response, convo) {
-        console.log(response);
-        convo.ask('明日は雨です。車で出かけられますか？', function (response, convo) {
-            console.log(response);
-            convo.say('承知しました。');
-            if (response.text == 'yes') {
-                askCar(response, convo);
-            }
-            convo.next();
-        });
-    };
 
-    askCar = function (response, convo) {
-        convo.ask('折りたたみ傘が必要ですか？', function (response, convo) {
-            if (response.text == 'yes') {
-                convo.say('承知いたしました。ご用意いたします。');
-            } else {
-                convo.say('お気をつけてください。');
-            }
-            convo.next();
-        });
-    };
-
-    bot.startConversation(message, askWeather);
-});
 controller.hears('おはよう', 'direct_message,direct_mention,mention',function(bot,message){
     bot.reply(message, 'おはようございます');
 });
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function (err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user, function (err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
-
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function (bot, message) {
-
-    controller.storage.users.get(message.user, function (err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function (err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function (response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function (response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function (response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function (response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, { 'key': 'nickname' }); // store the results in a field called nickname
-
-                    convo.on('end', function (convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function (err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function (err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
-
 
 controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function (bot, message) {
 
@@ -173,19 +56,6 @@ controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function
         ]);
     });
 });
-
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function (bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-    });
 
 function formatUptime(uptime) {
     var unit = 'second';
