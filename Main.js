eda05301a3f0589b92e914d08229e60acd7a9818
ATLAS0003 <norihiromatsuzaka@yahoo.co.jp>
2017-02-28 11:53:19 +0900

    Revert "「slack上の発言」に対して画像、emojiを返す仕組みを実装"
    This reverts commit b803ab16e6299d6c57d8ca0bdb21aa3f82bb18a1.

--- Main.js
+++ Main.js
@@ -1,9 +1,4 @@
 //Mcc_hackason slackおしゃべりbot
-//最初にtokenセットしなくても行けるように
-
-var tokene = "eG94Yi0xNDY2ODA0MTk2MzUtY05pRm5SaTlmeU8yZnZNck9uSHVJaHV5";
-var tokend = new Buffer(tokene, 'base64').toString('utf8');
-process.env.token = tokend;
 
 if (!process.env.token) {
     console.log('Error: Specify token in environment');
@@ -16,68 +11,48 @@ var os = require('os');
 var controller = Botkit.slackbot({
     debug: true,
 });
-//var ti = "xoxb-146680419635-cNiFnRi9fyO2fvMrOnHuIhuy";
-
-//環境変数process.env.tokenを代入
 
 var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
-
 //文字コード変換
 //var encoding = require('./encoding.js/encoding.js');
 //文字コード変換は使わないかもです
-//->使わないです
-//あとはpython待ち
 var controller = Botkit.slackbot({
     debug: false
 });
 controller.spawn({
     token: process.env.token
 }).startRTM();
-
 //すべての発言に反応する
-//直前にリプライ飛ばした人に対して@リアクション
-controller.hears(['REACTION'], 'direct_message,direct_mention,mention',function(bot,message){
-
-    controller.storage.users.get(message.user, function (err, user_info)
-    {
-        //@リアクション
-        //bot.reply(message, "<@" + message.user + ">");
-
-        //emojiReaction
-        bot.api.reactions.add({
-            timestamp: message.ts,
-            channel: message.channel,
-            name: '+1',
-        }, function (err, res) {
-            if (err) {
-                bot.botkit.log('Failed to add emoji reaction ', JSON.stringify(err));
-            }
-        });
-    });
-
-   });
-//直前にリプライ飛ばした人に対して画像を送る
-controller.hears(['IMAGE'], 'direct_message,direct_mention,mention', function (bot, message) {
-
+controller.hears('REACTION', 'direct_message,direct_mention,mention',function(bot,message){
+    
     //at reply
     controller.storage.users.get(message.user, function (err, user_info)
     {
         bot.reply(message, "<@" + message.user + ">");
     });
-    //画像読み込み
+
+   });
+
+controller.hears('IMAGE', 'direct_message,direct_mention,mention', function (bot, message) {
+
+    //at reply
+    /*controller.storage.users.get(message.user, function (err, user_info)
+    {
+        bot.reply(message, "<@" + message.user + ">");
+    });*/
     var fs = require('fs');
     bot.api.files.upload(
         {
-            file: fs.createReadStream('./image/hoge.png'),
+            file: fs.createReadStream('./hoge.png'),
             filename: 'hoge.png',
             channels: message.channel
         }, function (err, res) {
             if (err) console.log(err)
         });
 });
-//bot終了コマンド
+
 controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function (bot, message) {
 
     bot.startConversation(message, function (err, convo) {
